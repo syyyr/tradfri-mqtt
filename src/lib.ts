8b91ab55f227ffc896798782b07db62ffc714251
Václav Kubernát <sir.venceslas@gmail.com>
2020-11-18 01:57:26 +0100

    Refactor lib

--- src/lib.ts
+++ src/lib.ts
@@ -1,35 +1,45 @@
 import MQTT from "async-mqtt";
-interface Input {
+type MQTTVars = {
+    "friendly-name": string;
+    "broker-address": string;
+    client?: MQTT.AsyncMqttClient;
+};
+
+type LightState = {
     brightness: number;
     state: "on" | "off" | "toggle";
     warmth: number;
-    "friendly-name": string;
-    "broker-address": string;
+}
+
+type SendInput = MQTTVars & LightState;
+
+const getConnection = (input: MQTTVars) => {
+    return MQTT.connectAsync(`tcp://${input["broker-address"]}`);
+}
+
+const getClient = async (input: MQTTVars) => {
+    if (typeof input.client === "undefined") {
+        input.client = await getConnection(input);
+    }
+    return input.client;
 };
 
-const impl = async (client: MQTT.AsyncMqttClient, input: Input) => {
+const send = async (input: SendInput) => {
+    if (input.brightness > 254 || input.brightness < 0) {
+        console.error("Brightness must be within 0 and 254");
+        process.exit(1);
+    }
+    const client = await getClient(input);
     const message = {
         brightness: input.brightness,
         state: input.state,
         color_temp: input.warmth
     };
-    try {
-        await client.publish(`zigbee2mqtt/${input["friendly-name"]}/set`, JSON.stringify(message));
-        await client.end();
-    } catch (e){
-        console.log(e.stack);
-        process.exit();
-    }
-};
-
-const send = async (input: Input) => {
-    if (input.brightness > 254 || input.brightness < 0) {
-        console.error("Brightness must be within 0 and 254");
-        process.exit(1);
-    }
-    const client = await MQTT.connectAsync(`tcp://${input["broker-address"]}`);
-    impl(client, input);
+    await client.publish(`zigbee2mqtt/${input["friendly-name"]}/set`, JSON.stringify(message));
+    await client.end();
 }
+
+export {MQTTVars};
 
 export default {
     send
